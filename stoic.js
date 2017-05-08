b5fe8322e91482d8f3eb45566a6dcc3f94b9ca6d
Drew Smith <andrew.j.smith.jr@gmail.com>
2017-05-07 23:39:54 -0500

    using fetch API

--- stoic.js
+++ stoic.js
@@ -3,7 +3,9 @@
 const passage = document.getElementById('passage')
 const attribution = document.getElementById('attribution')
 
-fetch('http://google.com').then((response) => {
-    passage.innerHTML = response
-    attribution.innerHTML = 'Anonymous'
+fetch('https://d8ivtvzu8e.execute-api.us-west-2.amazonaws.com/prod/stoicChromeGetPassage').then((response) => {
+    response.json().then((json) => {
+        passage.innerHTML = json.passage
+        attribution.innerHTML = json.attribution || 'Anonymous'
+    })
 })
